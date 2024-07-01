a2b3fa7bcda968d09f8a9f47fa3a74a1cfa35137
Evgeniy Lobanov <evglbnv@yandex.ru>
2024-07-01 11:27:50 +0300

    Feature: Theme switcher slice

--- src/components/ThemeSwitcher/index.jsx
+++ src/components/ThemeSwitcher/index.jsx
@@ -1,7 +1,15 @@
-import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import { IoMoonOutline } from 'react-icons/io5';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles.module.css';
+import { setTheme } from '../../store/themeSlice';
 
 function ThemeSwitcher() {
+  const dispatch = useDispatch();
+
+  const theme = useSelector((state) => state.theme);
+
+  const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
+
   return (
     <div className={styles.ThemeSwitcher}>
       <IoMoonOutline></IoMoonOutline>
--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { coutriesReducer } from './countriesSlice';
 import { controlsReducer } from './controlsSlice';
 import { detailsReducer } from './detailsSlice';
+import { themeReducer } from './themeSlice';
 
 export const store = configureStore({
   reducer: {
     countries: coutriesReducer,
     controls: controlsReducer,
     details: detailsReducer,
+    theme: themeReducer,
   },
   devTools: true,
-  // middlware by default
 });

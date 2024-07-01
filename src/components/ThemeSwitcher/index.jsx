eb0fc0bb934ffaca9bdd2946a103de24ea131519
Evgeniy Lobanov <evglbnv@yandex.ru>
2024-07-01 17:36:14 +0300

    Feature: Theme switcher onClick activation

--- src/components/ThemeSwitcher/index.jsx
+++ src/components/ThemeSwitcher/index.jsx
@@ -1,5 +1,6 @@
-import { IoMoonOutline } from 'react-icons/io5';
+import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import styles from './styles.module.css';
 import { setTheme } from '../../store/themeSlice';
 
@@ -7,13 +8,18 @@ function ThemeSwitcher() {
   const dispatch = useDispatch();
 
   const theme = useSelector((state) => state.theme);
+  console.log(theme);
 
   const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
 
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
-    <div className={styles.ThemeSwitcher}>
-      <IoMoonOutline></IoMoonOutline>
-      <p className={styles.ThemeSwitcher__text}>Dark Mode</p>
+    <div className={styles.ThemeSwitcher} onClick={toggleTheme}>
+      {theme === 'light' ? (IoMoonOutline) : (IoMoon)}<IoMoonOutline></IoMoonOutline>
+      <p className={styles.ThemeSwitcher__text}>{theme} theme</p>
     </div>
   );
 }
